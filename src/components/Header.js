674cfd15a2bc5360a40246d456a0e77e339378e6
Rayni Nuñez <timetopwm@gmail.com>
2020-04-30 00:03:19 -0400

    Proyecto finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = ({ titulo }) => {
 
@@ -9,4 +10,8 @@ const Header = ({ titulo }) => {
      );
 }
  
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
+}
+
 export default Header;
